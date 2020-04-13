5a51a23ab1655ac0cb2c170421b3b568e6e59107
monster <1107743923@qq.com>
2020-04-13 11:24:46 +0800

    对Servlet的方法进行封装

---
+++ src/main/java/com/monster/travel/web/servlet/UserServlet.java
@@ -0,0 +1,209 @@
+package com.monster.travel.web.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.monster.travel.domain.ResultInfo;
+import com.monster.travel.domain.User;
+import com.monster.travel.service.UserService;
+import com.monster.travel.service.impl.UserServiceImpl;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet("/user/*")
+public class UserServlet extends BaseServlet {
+
+    private  UserService service = new UserServiceImpl();
+
+
+    /**
+     * 注册
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void regist(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String check = request.getParameter("check");
+        HttpSession session = request.getSession();
+        String checkcode_server = (String) session.getAttribute("CHECKCODE_SERVER");
+
+        //保证验证码只能使用一次
+        session.removeAttribute("CHECKCODE_SERVER");
+        if (checkcode_server == null || !checkcode_server.equalsIgnoreCase(check)) {
+            ResultInfo info = new ResultInfo();
+            info.setFlag(false);
+            info.setErrorMsg("验证码错误");
+
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(info);
+            response.setContentType("application/json; charset=UTF-8");
+            response.getWriter().write(json);
+
+            return;
+        }
+
+        Map<String, String[]> map = request.getParameterMap();
+        User user = new User();
+        try {
+            BeanUtils.populate(user, map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        boolean flag = service.regist(user);
+        ResultInfo info = new ResultInfo();
+        if (flag) {
+            info.setFlag(true);
+        } else {
+            info.setFlag(false);
+            info.setErrorMsg("注册失败");
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(info);
+
+        response.setContentType("application/json; charset=UTF-8");
+        response.getWriter().write(json);
+    }
+
+
+    /**
+     * 登录
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //验证码
+        String check = req.getParameter("check");
+        HttpSession session = req.getSession();
+        String checkcode_server = (String) session.getAttribute("CHECKCODE_SERVER");
+
+        //保证验证码只能使用一次
+        session.removeAttribute("CHECKCODE_SERVER");
+        if (checkcode_server == null || !checkcode_server.equalsIgnoreCase(check)) {
+            ResultInfo info = new ResultInfo();
+            info.setFlag(false);
+            info.setErrorMsg("验证码错误");
+
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(info);
+            resp.setContentType("application/json; charset=UTF-8");
+            resp.getWriter().write(json);
+            return;
+        }
+
+        //获取数据
+        Map<String, String[]> map = req.getParameterMap();
+
+        //封装user
+        User user = new User();
+        try {
+            BeanUtils.populate(user, map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+//        UserService service = new UserServiceImpl();
+        User u = service.login(user);
+
+        ResultInfo info = new ResultInfo();
+        //判断
+        if (u == null) {
+            //用户名 密码错误
+            info.setFlag(false);
+            info.setErrorMsg("用户名或密码错误");
+        }
+        if (u != null && !"Y".equals(u.getStatus())) {
+            //用户未激活
+            info.setFlag(false);
+            info.setErrorMsg("您未激活账号，请登录邮箱激活...");
+        }
+        if (u != null && "Y".equals(u.getStatus())) {
+            req.getSession().setAttribute("user", u);
+            //登录成功
+            info.setFlag(true);
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        resp.setContentType("application/json; charset=UTF-8");
+
+        mapper.writeValue(resp.getOutputStream(), info);
+    }
+
+
+    /**
+     * 查找一个用户
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void findOne(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //从session中获取
+        Object user = req.getSession().getAttribute("user");
+
+        ObjectMapper mapper = new ObjectMapper();
+        resp.setContentType("application/json; charset=UTF-8");
+        mapper.writeValue(resp.getOutputStream(), user);
+    }
+
+    /**
+     * 退出
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void exit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //销毁Session
+        req.getSession().invalidate();
+
+        //重定向到登录页面
+        resp.sendRedirect(req.getContextPath() + "/login.html");
+    }
+
+    /**
+     * 邮箱激活
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void active(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //获取激活码
+        String code = req.getParameter("code");
+
+        if (code != null) {
+            //调用service完成激活
+//            UserService service = new UserServiceImpl();
+            boolean flag = service.active(code);
+
+            String msg = null;
+            if (flag) {
+                msg = "激活成功，请<a href='login.html'>登录</a>";
+            } else {
+                msg = "激活失败，请联系管理员.....";
+            }
+            resp.setContentType("text/html; charset=UTF-8");
+            resp.getWriter().write(msg);
+        }
+    }
+}
